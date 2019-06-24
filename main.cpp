fd9744efd90dbcd54345c40bdeea45a66911bd29
Lennard Duinkerken <lennarduinkerken@gmail.com>
2019-06-24 11:48:14 +0200

    inital commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+// ==========================================================================
+//
+// blink a led on PIOB pin 27 == arduino-due on-board LED
+//
+// This file is in the public domain.
+//
+// ==========================================================================
+
+#include "hwlib.hpp"
+
+int main( void ){	
+   // hwlib will kill the watchdog
+   
+   // the on-board LED is connected to port B bit 27   
+   auto led = hwlib::target::pin_in_out( hwlib::target::pins::d13 );
+   led.direction_set_output();
+   led.direction_flush();
+   while(1){
+      led.write( 1 );
+      led.flush();
+      hwlib::wait_ms( 200 );
+      led.write( 0 );
+      led.flush();
+      hwlib::wait_ms( 200 );   
+   }
+}
